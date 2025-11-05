271cbaea009f22434d31deabd1f47519762a2765
정솔미 <solmi-@kookmin.ac.kr>
2025-11-05 11:35:02 +0900

    스크롤바없앰

--- component/dynamicLyric.tsx
+++ component/dynamicLyric.tsx
@@ -8,10 +8,8 @@ export default function DynamicLyric({
   lyric: LyricsProp[];
   current: string | null;
 }) {
-  // console.log('current from dynamicLyric', current)
-  
   return (
-    <div className="absolute w-[50rem] flex flex-col items-center text-center justify-center h-screen overflow-y-scroll pointer-events-none">{lyric.map(l => (
+    <div className="absolute w-[50rem] flex flex-col items-center text-center justify-center h-screen overflow-y-hidden pointer-events-none">{lyric.map(l => (
       <Fragment
         key={l.name}
       >
